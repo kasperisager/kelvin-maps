4fdbd1e221890c3570521ba0995c9597f87599ef
niklaspelle <niklaspelle@hotmail.com>
2015-03-02 16:19:40 +0100

    fixed code style

--- src/main/java/dk/itu/kelvin/controller/ChartController.java
+++ src/main/java/dk/itu/kelvin/controller/ChartController.java
@@ -450,9 +450,10 @@ public final class ChartController {
    * Will reset the compass, so it points north.
    */
   @FXML
-  private void compassReset(){
+  private void compassReset() {
     //to be continued
   }
+
   /**
    * Swap the text of the from and to address inputs.
    */
