4942f0a1702eac095ac12d18b4825da11b57bed3
Mathias Grundtvig Andreasen <mgru@itu.dk>
2015-03-03 11:52:30 +0100

    Fixed Checkstyle

--- src/main/java/dk/itu/kelvin/controller/MenuController.java
+++ src/main/java/dk/itu/kelvin/controller/MenuController.java
@@ -10,19 +10,23 @@ import java.io.File;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
+/**
+ * MenuBar controller class.
+ *
+ * @version 1.0.0
+ */
 public class MenuController {
   /**
    * Choose an .OSM file to be loaded.
    */
-  public void pickFile() {
-    FileChooser filechooser= new FileChooser();
+  public final void pickFile() {
+    FileChooser filechooser = new FileChooser();
     filechooser.setTitle("Select file to load");
     filechooser.getExtensionFilters().add(
-      new FileChooser.ExtensionFilter("Text Files", "*.osm")
+      new FileChooser.ExtensionFilter("All Files", "*.osm")
     );
     File file = filechooser.showOpenDialog(new Stage());
-    
-    if(file != null){
+    if (file != null) {
       //do something with the file
       System.out.println(file.getAbsolutePath());
     }
