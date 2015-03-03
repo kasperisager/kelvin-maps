60bff3269c1f50726fdedfddc307bb78091916d1
Mathias Grundtvig Andreasen <mgru@itu.dk>
2015-03-03 11:25:20 +0100

    Added FileChooser to menubar

--- src/main/java/dk/itu/kelvin/controller/MenuController.java
+++ src/main/java/dk/itu/kelvin/controller/MenuController.java
@@ -1,8 +1,27 @@
 package dk.itu.kelvin.controller;
 
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
 /**
  * Copyright (C) 2015 The Authors.
  */
+
+
 public class MenuController {
-  
+  /**
+   * Choose an .OSM file to be loaded.
+   */
+  public void pickFile() {
+    FileChooser filechooser= new FileChooser();
+    filechooser.setTitle("Select file to load");
+    filechooser.getExtensionFilters().add(
+        new FileChooser.ExtensionFilter("Text Files", "*.osm"));
+    File file = filechooser.showOpenDialog(new Stage());
+    if(file != null){
+      //do something with the file
+      System.out.println(file.getAbsolutePath());
+    }
+  }
 }
