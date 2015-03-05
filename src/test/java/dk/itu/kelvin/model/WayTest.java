9a8c9fd2cc11d78d2496d3d2880180920fed97b9
djohan <hjalte55@gmail.com>
2015-03-05 16:04:59 +0100

    Final config button updated

--- src/test/java/dk/itu/kelvin/model/WayTest.java
+++ src/test/java/dk/itu/kelvin/model/WayTest.java
@@ -9,24 +9,17 @@ import org.junit.Test;
 // JUnit assertions
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
 
 /**
  * Way unit tests.
  */
 public final class WayTest {
   public WayTest() {
-    Way w1 = new Way(25838049);
-    Node n1 = new Node(1712550893, (float)55.1198149, (float)12.1159972);
-    Node n2 = new Node(1712550893, (float)56.2298149, (float)16.2259972);
-    Node n3 = new Node(1712550893, (float)55.3398149, (float)12.3359972);
-    w1.node(n1);
-    w1.node(n2);
-    w1.node(n3);
-
-    Way w2 = new Way(31838449);
-    Node n4 = new Node(1712550893, (float)55.1198149, (float)12.1159972);
-    Node n5 = new Node(1712550893, (float)55.2298149, (float)12.2259972);
-    Node n6 = new Node(1712550893, (float)55.3398149, (float)12.3359972);
+    Way w2 = new Way(21838449);
+    Node n4 = new Node(1112550893, (float)57.1198149, (float)18.1159972);
+    Node n5 = new Node(1212550893, (float)58.2298149, (float)16.2259972);
+    Node n6 = new Node(1312550893, (float)59.3398149, (float)15.3359972);
     w2.node(n4);
     w2.node(n5);
     w2.node(n6);
@@ -37,12 +30,8 @@ public final class WayTest {
    */
   @Test
   public void testTag() {
-    // Test that unspecified tags are not recognized.
-    Way w3 = new Way(25338049);
-    w3.tag("SuperBrugsen", null);
-    assertEquals(0, w3.tags().size());
-
     // Test that a tag is recognized and added to the Way object.
+    Way w3 = new Way(25338049);
     w3.tag("building", null);
     assertEquals(1, w3.tags().size());
     assertTrue(w3.tags().containsKey("building"));
@@ -53,5 +42,122 @@ public final class WayTest {
     assertTrue(w3.tags().containsKey("highway"));
     assertEquals("primary", w3.tags().get("highway"));
   }
+
+  /**
+   * Test if the closed method recognizes closed ways and unclosed ways.
+   */
+  @Test
+  public void testClosed() {
+    Way w1 = new Way(15838049);
+    Node n1 = new Node(1112550893, (float)55.1198149, (float)12.1159972);
+    Node n2 = new Node(1212550893, (float)56.2298149, (float)17.2259972);
+    Node n3 = new Node(1312550893, (float)57.3398149, (float)18.3359972);
+    Node n4 = new Node(1312550893, (float)55.1198149, (float)12.1159972);
+    w1.node(n1);
+    w1.node(n2);
+    w1.node(n3);
+    w1.node(n4);
+
+    assertTrue(w1.closed());
+
+    Way w2 = new Way(15838049);
+    Node n5 = new Node(1112550893, (float)55.1198149, (float)12.1159972);
+    Node n6 = new Node(1212550893, (float)56.2298149, (float)17.2259972);
+    Node n7 = new Node(1312550893, (float)57.3398149, (float)18.3359972);
+    Node n8 = new Node(1312550893, (float)55.4498149, (float)12.4459972);
+    w2.node(n5);
+    w2.node(n6);
+    w2.node(n7);
+    w2.node(n8);
+
+    assertFalse(w2.closed());
+  }
+
+  /**
+   * StartsIn should only return true if the current
+   * * Way starts in the same coordinates as the node in the parameter ends or starts in.
+   */
+  @Test
+  public void testStartsIn() {
+    // Way w1 and Way w2 starts in the same coordinates.
+    Way w1 = new Way(15838049);
+    Node n1 = new Node(1112550893, (float)55.1198149, (float)12.1159972);
+    Node n2 = new Node(1212550893, (float)56.2298149, (float)17.2259972);
+    Node n3 = new Node(1312550893, (float)57.3398149, (float)18.3359972);
+    Node n4 = new Node(1412550893, (float)55.1198149, (float)12.1159972);
+    w1.node(n1);
+    w1.node(n2);
+    w1.node(n3);
+    w1.node(n4);
+
+    Way w2 = new Way(15838049);
+    Node n5 = new Node(1512550893, (float)55.1198149, (float)12.1159972);
+    Node n6 = new Node(1612550893, (float)56.2298149, (float)17.2259972);
+    Node n7 = new Node(1712550893, (float)57.3398149, (float)18.3359972);
+    Node n8 = new Node(1812550893, (float)55.4498149, (float)12.4459972);
+    w2.node(n5);
+    w2.node(n6);
+    w2.node(n7);
+    w2.node(n8);
+
+    assertTrue(w1.startsIn(w2));
+    assertFalse(w1.startsIn(null));
+
+    Way w3 = new Way(15838049);
+    Node n9 = new Node(1512550893, (float)59.1198149, (float)18.1159972);
+    Node n10 = new Node(1612550893, (float)56.2298149, (float)17.2259972);
+    Node n11 = new Node(1712550893, (float)57.3398149, (float)18.3359972);
+    Node n12 = new Node(1112550893, (float)55.1198149, (float)12.1159972);
+    w3.node(n9);
+    w3.node(n10);
+    w3.node(n11);
+    w3.node(n12);
+
+    // w1 starts in the same coordinates as w3 ends in.
+    assertTrue(w1.startsIn(w3));
+    // w3 ends in the same coordinates as w1 but does not start in it.
+    assertFalse(w3.startsIn(w1));
+  }
+
+  @Test
+  public void testEndsIn() {
+    // Way w1 and Way w2 starts in the same coordinates.
+    Way w1 = new Way(15838049);
+    Node n1 = new Node(1112550893, (float)55.1198149, (float)12.1159972);
+    Node n2 = new Node(1212550893, (float)56.2298149, (float)17.2259972);
+    Node n3 = new Node(1312550893, (float)57.3398149, (float)18.3359972);
+    w1.node(n1);
+    w1.node(n2);
+    w1.node(n3);
+
+    Way w2 = new Way(15838049);
+    Node n4 = new Node(1112550893, (float)57.3398149, (float)18.3359972);
+    Node n5 = new Node(1612550893, (float)56.2298149, (float)17.2259972);
+    Node n6 = new Node(1712550893, (float)50.3398149, (float)10.3359972);
+    w2.node(n4);
+    w2.node(n5);
+    w2.node(n6);
+
+    // w1 ends in the same coordinates as w2 starts in.
+    assertTrue(w1.endsIn(w2));
+    // w2 does not end in the same coordinates as w1 starts or ends in.
+    assertFalse(w2.endsIn(w1));
+    assertFalse(w1.endsIn(null));
+
+    Way w3 = new Way(15838049);
+    Node n9 = new Node(1512550893, (float)59.1198149, (float)18.1159972);
+    Node n10 = new Node(1612550893, (float)56.2298149, (float)17.2259972);
+    Node n11 = new Node(1712550893, (float)50.3398149, (float)10.3359972);
+    Node n12 = new Node(1112550893, (float)57.3398149, (float)18.3359972);
+    w3.node(n9);
+    w3.node(n10);
+    w3.node(n11);
+    w3.node(n12);
+
+    // w1 ends in the same coordinates as w3 ends in.
+    assertTrue(w1.endsIn(w3));
+  }
+
+
 
 }
