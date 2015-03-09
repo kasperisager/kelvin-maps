e248107cbfa275ccfdbb19eaf170f477e97d5cc5
Kasper Isager <kasperisager@gmail.com>
2015-03-09 17:04:07 +0100

    Adjust way spec.

--- src/test/java/dk/itu/kelvin/model/WayTest.java
+++ src/test/java/dk/itu/kelvin/model/WayTest.java
@@ -28,13 +28,13 @@ public final class WayTest {
     // Test that a tag is recognized and added to the Way object.
     Way w3 = new Way(25338049);
     w3.tag("building", null);
-    assertEquals(1, w3.tags().size());
-    assertTrue(w3.tags().containsKey("building"));
+    assertEquals(0, w3.tags().size());
+    assertFalse(w3.tags().containsKey("building"));
 
     // Test that key and value type tags
     // are recognized and added to the Way object.
     w3.tag("highway", "primary");
-    assertEquals(2, w3.tags().size());
+    assertEquals(1, w3.tags().size());
     assertTrue(w3.tags().containsKey("highway"));
     assertEquals("primary", w3.tags().get("highway"));
   }
