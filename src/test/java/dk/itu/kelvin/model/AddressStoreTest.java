235ca94dbd19a684d699e5899349494a2af97460
djohan <hjalte55@gmail.com>
2015-03-10 13:34:34 +0100

    Address equals and hashcode method. AddressStoreTest2

--- src/test/java/dk/itu/kelvin/model/AddressStoreTest.java
+++ src/test/java/dk/itu/kelvin/model/AddressStoreTest.java
@@ -1,7 +1,25 @@
+/**
+ * Copyright (C) 2015 The Authors.
+ */
 package dk.itu.kelvin.model;
 
-/**
- * Created by djohan on 10/03/15.
- */
+// JUnit annotations
+import org.junit.Test;
+
+// JUnit assertions
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
 public class AddressStoreTest {
+
+  @Test
+  public void testEquals() {
+    Address a1 = new Address();
+    
+
+  }
+
+
+
 }
