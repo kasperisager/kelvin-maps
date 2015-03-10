b6d9137e4c44876f6b01c264930d872b5c15b564
Kasper Isager <kasperisager@gmail.com>
2015-03-10 22:36:41 +0100

    Refactor main application class.

--- src/main/java/dk/itu/kelvin/Main.java
+++ src/main/java/dk/itu/kelvin/Main.java
@@ -7,7 +7,6 @@ package dk.itu.kelvin;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.Parent;
 
 // JavaFX text utilities
 import javafx.scene.text.Font;
@@ -22,35 +21,122 @@ import javafx.fxml.FXMLLoader;
  */
 public final class Main extends Application {
   /**
-   * Start the JavaFX thread and hand off control to the primary controller.
+   * The title of the application.
    *
-   * @param primaryStage The primary stage of the application.
+   * This will be shown in the title bar of the application window.
+   */
+  private static final String TITLE = "Kelvin Maps";
+
+  /**
+   * The minimum width of the application window.
+   */
+  private static final int MIN_WIDTH = 500;
+
+  /**
+   * The minimum height of the application window.
+   */
+  private static final int MIN_HEIGHT = 300;
+
+  /**
+   * The main view of the application.
    *
-   * @throws Exception In case of an error.
+   * This view be loaded and used as the main entry point to the application.
+   */
+  private static final String MAIN_VIEW = "view/Application.fxml";
+
+  /**
+   * Stylesheets to load in the application.
+   */
+  private static final String[] STYLESHEETS = new String[] {
+    "stylesheet/Main.css"
+  };
+
+  /**
+   * Fonts to load in the application.
+   */
+  private static final String[] FONTS = new String[] {
+    "font/ionicons.ttf"
+  };
+
+  /**
+   * The scene of the application.
+   */
+  private Scene scene;
+
+  /**
+   * Load the main scene of the application.
+   */
+  private void loadScene() {
+    // Bail out if the scene has already been loaded.
+    if (this.scene != null) {
+      return;
+    }
+
+    try {
+      this.scene = new Scene(
+        (new FXMLLoader()).load(this.getClass().getResource(MAIN_VIEW))
+      );
+    }
+    catch (Exception ex) {
+      // This should never happen so simply propagate the exception as a
+      // runtime exception.
+      throw new RuntimeException(ex);
+    }
+  }
+
+  /**
+   * Load all application stylesheets.
+   */
+  private void loadStylesheets() {
+    // Attempt loading the scene if it hasn't already been loaded.
+    if (this.scene == null) {
+      this.loadScene();
+    }
+
+    for (String stylesheet: STYLESHEETS) {
+      this.scene.getStylesheets().add(
+        this.getClass().getResource(stylesheet).toExternalForm()
+      );
+    }
+  }
+
+  /**
+   * Load all application fonts.
+   */
+  private void loadFonts() {
+    for (String font: FONTS) {
+      Font.loadFont(this.getClass().getResourceAsStream(font), 14);
+    }
+  }
+
+  /**
+   * Start the JavaFX thread and hand off control to the main view.
+   *
+   * @param stage The primary stage of the application.
    */
   @Override
-  public void start(final Stage primaryStage) throws Exception {
-    FXMLLoader fxmlLoader = new FXMLLoader();
+  public void start(final Stage stage) {
+    // 1. Set the application title.
+    stage.setTitle(TITLE);
 
-    Parent root = fxmlLoader.load(
-      this.getClass().getResource("view/Application.fxml")
-    );
+    // 2. Set the minimum dimensions of the application window.
+    stage.setMinWidth(MIN_WIDTH);
+    stage.setMinHeight(MIN_HEIGHT);
 
-    Scene scene = new Scene(root);
+    // 3. Load the main scene of the application.
+    this.loadScene();
 
-    Font.loadFont(
-      this.getClass().getResourceAsStream("font/ionicons.ttf"), 14
-    );
+    // 4. Load the application fonts.
+    this.loadFonts();
 
-    scene.getStylesheets().add(
-      this.getClass().getResource("stylesheet/Main.css").toExternalForm()
-    );
+    // 5. Load the application stylesheets.
+    this.loadStylesheets();
 
-    primaryStage.setTitle("Kelvin Maps");
-    primaryStage.setScene(scene);
-    primaryStage.setMinHeight(500);
-    primaryStage.setMinWidth(300);
-    primaryStage.show();
+    // 6. Set the scene of the primary stage.
+    stage.setScene(this.scene);
+
+    // 7. Show the primary stage. Eureka!
+    stage.show();
   }
 
   /**
