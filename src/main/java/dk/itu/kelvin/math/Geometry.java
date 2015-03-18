c1b29132c2a83e76971477b4df05601ef716d0f3
Kasper Isager <kasperisager@gmail.com>
2015-03-18 11:36:09 +0100

    Add Bounds class to Geometry. This commit also adds some extra methods to Epsilon for comparing floating points.

--- src/main/java/dk/itu/kelvin/math/Geometry.java
+++ src/main/java/dk/itu/kelvin/math/Geometry.java
@@ -26,75 +26,84 @@ public final class Geometry {
   }
 
   /**
-   * Calculate the line-line intersection between the specified line segments.
+   * Check if two bounds intersects.
    *
-   * <p>
-   * This algorithm is based on an implementation of the line-line intersection
-   * algorithm by Alexander Hristov.
-   *
-   * @see <a href="http://goo.gl/f7uPdH">Line-line intersection</a>
-   * @see <a href="http://goo.gl/Z7cksG">Intersection of lines in 2D</a>
-   *
-   * @param p1  The starting point of the first line.
-   * @param p2  The ending point of the first line.
-   * @param p3  The starting point of the second line.
-   * @param p4  The ending point of the second line.
-   * @return    Point where the segments intersect, or {@code null} if they
-   *            don't.
+   * @param a The first bounds.
+   * @param b The second bounds.
+   * @return  A boolean indicating whether or not the bounds intersect.
    */
-  public static Point intersection(
-    final Point p1,
-    final Point p2,
-    final Point p3,
-    final Point p4
-  ) {
-    float x1 = p1.x();
-    float y1 = p1.y();
-    float x2 = p2.x();
-    float y2 = p2.y();
-    float x3 = p3.x();
-    float y3 = p3.y();
-    float x4 = p4.x();
-    float y4 = p4.y();
-
-    // Calculate the denominator of the line segments.
-    float d = ((x1 - x2) * (y3 - y4)) - ((y1 - y2) * (x3 - x4));
-
-    // If the denominator is zero the lines do not intersect.
-    if (Epsilon.equal(d, 0.0f)) {
-      return null;
-    }
-
-    // Calculate the x-coordinate of the intersection.
-    float xi = (
-      (x3 - x4) * (x1 * y2 - y1 * x2) - (x1 - x2) * (x3 * y4 - y3 * x4)
-    ) / d;
-
-    // Calculate the y-coordinate of the intersection.
-    float yi = (
-      (y3 - y4) * (x1 * y2 - y1 * x2) - (y1 - y2) * (x3 * y4 - y3 * x4)
-    ) / d;
-
-    if (xi < Math.min(x1, x2) || xi > Math.max(x1, x2)) {
-      return null;
-    }
-
-    if (xi < Math.min(x3, x4) || xi > Math.max(x3, x4)) {
-      return null;
-    }
-
-    return new Point(xi, yi);
+  public static boolean intersects(final Bounds a, final Bounds b) {
+    return (
+      a.left <= b.right
+      && a.right >= b.left
+      && a.top <= b.bottom
+      && a.bottom >= b.top
+    );
   }
 
   /**
    * Calculate the line-line intersection between the specified line segments.
+   *
+   * @see <a href="http://goo.gl/f7uPdH">Line-line intersection</a>
    *
    * @param a The first line segment.
    * @param b The second line segment.
    * @return  Point where the segments intersect, or {@code null} if they don't.
    */
   public static Point intersection(final Line a, final Line b) {
-    return Geometry.intersection(a.start(), a.end(), b.start(), b.end());
+    float d = (
+      (a.start.x - a.end.x) * (b.start.y - b.end.y) -
+      (a.start.y - a.end.y) * (b.start.x - b.end.x)
+    );
+
+    if (Epsilon.equals(d, 0.0f)) {
+      return null;
+    }
+
+    double px = (
+      (b.end.x - b.start.x) * (a.start.y - b.start.y) -
+      (b.end.y - b.start.y) * (a.start.x - b.start.x)
+    ) / d;
+
+    double py = (
+      (a.end.x - a.start.x) * (a.start.y - b.start.y) -
+      (a.end.y - a.start.y) * (a.start.x - b.start.x)
+    ) / d;
+
+    if (
+      (Epsilon.less(px, 0.0f) || Epsilon.greater(px, 1.0f))
+      &&
+      (Epsilon.less(py, 0.0f) || Epsilon.greater(py, 1.0f))
+    ) {
+      return null;
+    }
+
+    Point p = new Point(
+      (float) (a.start.x + px * (a.end.x - a.start.x)),
+      (float) (a.start.y + py * (a.end.y - a.start.y))
+    );
+
+    if (
+      Epsilon.less(p.x, Math.min(a.start.x, a.end.x))
+      ||
+      Epsilon.greater(p.x, Math.max(a.start.x, a.end.x))
+      ||
+      Epsilon.less(p.x, Math.min(b.start.x, b.end.x))
+      ||
+      Epsilon.greater(p.x, Math.max(b.start.x, b.end.x))
+      ||
+      Epsilon.less(p.y, Math.min(a.start.y, a.end.y))
+      ||
+      Epsilon.greater(p.y, Math.max(a.start.y, a.end.y))
+      ||
+      Epsilon.less(p.y, Math.min(b.start.y, b.end.y))
+      ||
+      Epsilon.greater(p.y, Math.max(b.start.y, b.end.y))
+    ) {
+      return null;
+    }
+
+    return p;
   }
 
   /**
@@ -156,8 +165,8 @@ public final class Geometry {
 
     @Override
     public String toString() {
-      return "[point: "
-      + " x = " + this.x
+      return "Point["
+      + "x = " + this.x
       + ", y = " + this.y
       + "]";
     }
@@ -220,10 +229,28 @@ public final class Geometry {
     //   return Geometry.distance(this.start, this.end);
     // }
 
+    /**
+     * Check if the line is vertical.
+     *
+     * @return A boolean indicating whether or not the line is vertical.
+     */
+    public boolean isVertical() {
+      return Epsilon.equals(this.start.x, this.end.x);
+    }
+
+    /**
+     * Check if the line is horizontal.
+     *
+     * @return A boolean indicating whether or not the line is horizontal.
+     */
+    public boolean isHorizontal() {
+      return Epsilon.equals(this.start.y, this.end.y);
+    }
+
     @Override
     public String toString() {
-      return "[line: "
-      + " start = " + this.start
+      return "Line["
+      + "start = " + this.start
       + ", end = " + this.end
       + "]";
     }
@@ -306,8 +333,8 @@ public final class Geometry {
 
     @Override
     public String toString() {
-      return "[circle:"
-      + " center = " + this.center
+      return "Circle["
+      + "center = " + this.center
       + ", radius = " + this.radius
       + "]";
     }
@@ -392,11 +419,135 @@ public final class Geometry {
 
     @Override
     public String toString() {
-      return "[rectangle: "
-      + " position = " + this.position
+      return "Rectangle["
+      + "position = " + this.position
       + ", width = " + this.width
       + ", height = " + this.height
       + "]";
+    }
+  }
+
+  /**
+   * The {@link Bounds} class describes the rectangular, geometric bounds of a
+   * 2-dimensional shape.
+   *
+   * <p>
+   * <b>OBS:</b> This class is only meant to be used for intermediate results.
+   * It should never be stored anywhere other than a local variable at most.
+   */
+  public static final class Bounds {
+    /**
+     * The top-most position of the bounds.
+     */
+    private final float top;
+
+    /**
+     * The right-most position of the bounds.
+     */
+    private final float right;
+
+    /**
+     * The bottom-most position of the bounds.
+     */
+    private final float bottom;
+
+    /**
+     * The left-most position of the bounds.
+     */
+    private final float left;
+
+    /**
+     * Initialize a set of bounds.
+     *
+     * @param top     The top-most position of the bounds.
+     * @param right   The right-most position of the bounds.
+     * @param bottom  The bottom-most position of the bounds.
+     * @param left    The left-most position of the bounds.
+     */
+    private Bounds(
+      final float top,
+      final float right,
+      final float bottom,
+      final float left
+    ) {
+      this.top = top;
+      this.right = right;
+      this.bottom = bottom;
+      this.left = left;
+    }
+
+    /**
+     * Initialize the bounds of a {@link Line}.
+     *
+     * @param line The line whose bounds to initialize.
+     */
+    public Bounds(final Line line) {
+      if (line.start.x > line.end.x) {
+        this.right = line.end().x();
+        this.left = line.start().x();
+      }
+      else {
+        this.right = line.start().x();
+        this.left = line.end().x();
+      }
+
+      if (line.start.y > line.end.y) {
+        this.top = line.start().y();
+        this.bottom = line.end().y();
+      }
+      else {
+        this.top = line.end().y();
+        this.bottom = line.start().y();
+      }
+    }
+
+    /**
+     * Initialize the bounds of a {@link Rectangle}.
+     *
+     * @param rectangle The rectangle whose bounds to initialize.
+     */
+    public Bounds(final Rectangle rectangle) {
+      this.left = rectangle.position().x();
+      this.right = this.left + rectangle.width();
+
+      this.top = rectangle.position().y();
+      this.bottom = this.top + rectangle.height();
+    }
+
+    /**
+     * Get the top-most position of the bounds.
+     *
+     * @return The top-most position of the bounds.
+     */
+    public float top() {
+      return this.top;
+    }
+
+    /**
+     * Get the right-most position of the bounds.
+     *
+     * @return The right-most position of the bounds.
+     */
+    public float right() {
+      return this.right;
+    }
+
+    /**
+     * Get the bottom-most position of the bounds.
+     *
+     * @return The bottom-most position of the bounds.
+     */
+    public float bottom() {
+      return this.bottom;
+    }
+
+    /**
+     * Get the left-most position of the bounds.
+     *
+     * @return The left-most position of the bounds.
+     */
+    public float left() {
+      return this.left;
     }
   }
 }
