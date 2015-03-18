409cbeb659ecc4456b3bdb239c52297de6df3ec8
Kasper Isager <kasperisager@gmail.com>
2015-03-18 18:15:58 +0100

    Implement algorithm for creating land polygons.

--- src/main/java/dk/itu/kelvin/model/Land.java
+++ src/main/java/dk/itu/kelvin/model/Land.java
@@ -4,12 +4,20 @@
 package dk.itu.kelvin.model;
 
 // General utilities
-import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.List;
 
 // JavaFX scene utilities
 import javafx.scene.Group;
+
+// JavaFX shapes
+import javafx.scene.shape.Polyline;
+
+// Utilities
+import dk.itu.kelvin.util.ArrayList;
+import dk.itu.kelvin.util.List;
+
+// Math
+import dk.itu.kelvin.math.Geometry;
 
 /**
  * Land class.
@@ -23,16 +31,6 @@ public final class Land extends Element<Group> {
   private static final long serialVersionUID = 81;
 
   /**
-   * The JavaFX representation of the land.
-   *
-   * <p>
-   * This field is transient as it is simply used for caching the rendered
-   * JavaFX scene graph node. We therefore don't want to store it when
-   * serializing the element.
-   */
-  private transient Group fx;
-
-  /**
    * Bounds of the land element.
    */
   private BoundingBox bounds;
@@ -43,23 +41,12 @@ public final class Land extends Element<Group> {
   private List<Way> coastlines = new ArrayList<>();
 
   /**
-   * Intersections between the coastlines and the bounds of the land.
-   */
-  // private Map<Node, Intersection> intersections = new HashMap<>();
-
-  public Land() {
-    super(Long.MAX_VALUE);
-  }
-
-  /**
    * Initialize a land instance with initial bounds.
    *
    * @param bounds The bounds of the land.
    */
   public Land(final BoundingBox bounds) {
-    this();
     this.bounds = bounds;
-    this.layer(-999);
   }
 
   /**
@@ -81,7 +68,8 @@ public final class Land extends Element<Group> {
       return;
     }
 
-    Way coastline = way;
+    Way coastline = new Way();
+    coastline.append(way);
 
     Iterator<Way> i = this.coastlines.iterator();
 
@@ -103,12 +91,51 @@ public final class Land extends Element<Group> {
   }
 
   /**
-   * Get the coastlines contained within the land.
+   * Construct geometric line segments that represent the bounds of the land
+   * mass.
    *
-   * @return The coastlines contained within the land.
+   * @return Geometric line segments that represent the bounds of the land mass.
    */
-  public List<Way> coastlines() {
-    return this.coastlines;
+  private Geometry.Line[] constructBounds() {
+    return new Geometry.Line[] {
+      // Left bounds segment.
+      new Geometry.Line(
+        new Geometry.Point(this.bounds.left(), this.bounds.top()),
+        new Geometry.Point(this.bounds.left(), this.bounds.bottom())
+      ),
+
+      // Top bounds segment.
+      new Geometry.Line(
+        new Geometry.Point(this.bounds.left(), this.bounds.top()),
+        new Geometry.Point(this.bounds.right(), this.bounds.top())
+      ),
+
+      // Right bounds segment.
+      new Geometry.Line(
+        new Geometry.Point(this.bounds.right(), this.bounds.bottom()),
+        new Geometry.Point(this.bounds.right(), this.bounds.top())
+      ),
+
+      // Bottom bounds segment.
+      new Geometry.Line(
+        new Geometry.Point(this.bounds.right(), this.bounds.bottom()),
+        new Geometry.Point(this.bounds.left(), this.bounds.bottom())
+      )
+    };
+  }
+
+  /**
+   * Construct a geometric line segment between the specified nodes.
+   *
+   * @param a The first node.
+   * @param b The second node.
+   * @return  A geometric line segment between the specified nodes.
+   */
+  private Geometry.Line constructLine(final Node a, final Node b) {
+    return new Geometry.Line(
+      new Geometry.Point(a.x(), a.y()),
+      new Geometry.Point(b.x(), b.y())
+    );
   }
 
   /**
@@ -117,26 +144,142 @@ public final class Land extends Element<Group> {
    * @return The JavaFX representation of the land.
    */
   public Group render() {
-    if (this.fx != null) {
-      return this.fx;
-    }
-
     Group group = new Group();
 
-    this.fx = group;
+    Geometry.Line[] bounds = this.constructBounds();
 
-    return this.fx;
-  }
+    for (Way coastline: this.coastlines) {
+      if (coastline.open()) {
+        Node previous1 = null;
+        Node previous2 = null;
 
-  /**
-   * Enumerator describing the intersections between coastlines and the land
-   * bounds.
-   */
-  private static enum Intersection {
-    /** Intersection of bounds and start node. */
-    START,
+        boolean inside1 = false;
+        boolean inside2 = false;
 
-    /** Intersection of bounds and end node. */
-    END;
+        List<Node> nodes = coastline.nodes();
+
+        int n = nodes.size();
+        int i = 0;
+
+        while (i < n) {
+          Node next = nodes.get(i++);
+
+          if (this.bounds.contains(next)) {
+            if (inside1 || previous1 == null) {
+              continue;
+            }
+
+            Geometry.Line line = this.constructLine(previous1, next);
+
+            for (Geometry.Line bound: bounds) {
+              Geometry.Point p;
+
+              if (bound.isVertical()) {
+                p = Geometry.intersection(line, bound);
+              }
+              else {
+                p = Geometry.intersection(bound, line);
+              }
+
+              if (p != null) {
+                Node node = new Node(p.x(), p.y());
+
+                nodes.add(i - 1, node);
+                i++;
+                n++;
+
+                if (bound == bounds[0]) {
+                  nodes.add(0, new Node(
+                    bound.end().x(),
+                    bound.end().y()
+                  ));
+                }
+                else {
+                  nodes.add(0, new Node(
+                    bound.start().x(),
+                    bound.start().y()
+                  ));
+                }
+
+                break;
+              }
+            }
+
+            inside1 = true;
+          }
+          else {
+            if (!inside1) {
+              nodes.remove(i - 1);
+              i--;
+              n--;
+            }
+
+            inside1 = false;
+          }
+
+          previous1 = next;
+        }
+
+        int j = n - 1;
+
+        while (j >= 0) {
+          Node next = nodes.get(j--);
+
+          if (this.bounds.contains(next)) {
+            if (inside2 || previous2 == null) {
+              continue;
+            }
+
+            Geometry.Line line = this.constructLine(previous2, next);
+
+            Node intersection = null;
+
+            for (Geometry.Line bound: bounds) {
+              Geometry.Point p;
+
+              if (bound.isVertical()) {
+                p = Geometry.intersection(line, bound);
+              }
+              else {
+                p = Geometry.intersection(bound, line);
+              }
+
+              if (p != null) {
+                Node node = new Node(p.x(), p.y());
+
+                nodes.add(j + 3, node);
+                j++;
+
+                nodes.add(new Node(
+                  bound.end().x(),
+                  bound.end().y()
+                ));
+
+                break;
+              }
+            }
+
+            inside2 = true;
+          }
+          else {
+            if (!inside2) {
+              nodes.remove(j + 1);
+              j--;
+            }
+
+            inside2 = false;
+          }
+
+          previous2 = next;
+        }
+      }
+
+      Polyline polyline = coastline.render();
+      polyline.getStyleClass().add("land");
+
+      group.getChildren().add(polyline);
+    }
+
+    return group;
   }
 }
