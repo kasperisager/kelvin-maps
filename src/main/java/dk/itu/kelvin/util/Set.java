0043297d3051a3380cff0136719ea2c066d0f65f
Mathias Grundtvig Andreasen <mgru@itu.dk>
2015-03-19 11:49:29 +0100

    Added documentation for Set

--- src/main/java/dk/itu/kelvin/util/Set.java
+++ src/main/java/dk/itu/kelvin/util/Set.java
@@ -4,7 +4,17 @@
 package dk.itu.kelvin.util;
 
 /**
- * Set interface.
+ * <h2>Sightly simplified implementation of Set interface</h2>
+ * <p>
+ * A {@link Collection} that contains no duplicate elements. More formally,
+ * sets contain no pair of elements {@code e1} and {@code e2} such that
+ * {@code e1.equals(e2)}, and at most one {@code null}, even though there
+ * isn't any implementing classes that allow {@code null elements}. As implied
+ * by its name, this interface models the mathematical set abstraction.
+ *
+ * <p>
+ * {@code Set} doesn't specify any additional methods that hasn't been
+ * specified in the {@link Collection} interface.
  *
  * @param <E> The type of elements contained within the set.
  *
