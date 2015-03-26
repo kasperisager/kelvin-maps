30ead77bead89172e6c33ee46e2fe266ac972b30
Kasper Isager <kasperisager@gmail.com>
2015-03-26 11:19:33 +0100

    Move method.

--- src/main/java/dk/itu/kelvin/math/Geometry.java
+++ src/main/java/dk/itu/kelvin/math/Geometry.java
@@ -122,22 +122,6 @@ public final class Geometry {
   }
 
   /**
-   * Calculate the distance between the specified points.
-   *
-   * @param a The first point.
-   * @param b The second point.
-   * @return  The distance between points {@code a} and {@code b} or {@code -1}
-   *          if either of them are {@code null}.
-   */
-  private static double distance(final Point a, final Point b) {
-    if (a == null || b == null) {
-      return -1;
-    }
-
-    return Math.sqrt(Math.pow(b.x - a.x, 2) + Math.pow(b.y - a.y, 2));
-  }
-
-  /**
    * The {@link Point} class describes a coordinate in 2-dimensional space.
    *
    * <p>
