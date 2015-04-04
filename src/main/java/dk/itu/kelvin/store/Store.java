54979a2b0d6d9261bf91932613b4210be0b525ec
Kasper Isager <kasperisager@gmail.com>
2015-04-04 14:29:04 +0200

    Initial implementation of Store class.

---
+++ src/main/java/dk/itu/kelvin/store/Store.java
@@ -0,0 +1,64 @@
+/**
+ * Copyright (C) 2015 The Authors.
+ */
+package dk.itu.kelvin.store;
+
+// I/O utilities
+import java.io.File;
+import java.io.Serializable;
+
+// Utilities
+import dk.itu.kelvin.util.List;
+
+// Functional utilities
+import dk.itu.kelvin.util.function.Callback;
+
+/**
+ * Store class.
+ *
+ * @param <E> The type of elements contained within the store.
+ * @param <C> The search criteria to use when looking up elements.
+ */
+public abstract class Store<E, C> implements Serializable {
+  /**
+   * Add an element to the store.
+   *
+   * @param element The element to add to the store.
+   */
+  public abstract void add(final E element);
+
+  /**
+   * Remove an element from the store.
+   *
+   * @param element The element to remove from the store.
+   */
+  public abstract void remove(final E element);
+
+  /**
+   * Look up elements within the store based on the specified criteria.
+   *
+   * @param criteria  The criteria to look up elements based on.
+   * @return          A list of elements matching the search criteria.
+   */
+  public abstract List<E> search(final C criteria);
+
+  /**
+   * Save the current store to the specified file.
+   *
+   * @param file      The file to save the store to.
+   * @param callback  The callback to invoke once saving has finished.
+   */
+  public final void save(final File file, final Callback callback) {
+    throw new UnsupportedOperationException();
+  }
+
+  /**
+   * Load a store saved to disk into the current store.
+   *
+   * @param file      The file containing the saved store.
+   * @param callback  The callback to invoke once loading has finished.
+   */
+  public final void load(final File file, final Callback callback) {
+    throw new UnsupportedOperationException();
+  }
+}
