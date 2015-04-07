f9adf9b4a2242ca7b0ca4e534a294f41a4f0833b
Mathias Grundtvig Andreasen <mgru@itu.dk>
2015-04-07 22:30:54 +0200

    Initial refactor, new fxml & controller

---
+++ src/main/java/dk/itu/kelvin/controller/AddressFieldController.java
@@ -0,0 +1,51 @@
+package dk.itu.kelvin.controller;
+
+import javafx.fxml.FXML;
+
+/**
+ * Created by Mathias on 07/04/2015.
+ */
+public class AddressFieldController {
+
+
+
+  public void initialize(){
+
+
+  }
+
+  @FXML
+  public void findAddress(){
+
+
+  }
+  @FXML
+  public void findRoute(){
+
+
+  }
+
+  @FXML
+  public void swapTextFields(){
+
+
+  }
+
+  @FXML
+  public void routeByCar(){
+
+  }
+  @FXML
+  public void routeByFoot(){
+
+  }
+
+  @FXML
+  public void hidePOI(){
+
+  }
+  @FXML
+  public void hideDirections(){
+
+  }
+}
