59871ee4c53a7c3841b320c3c33f4082e8125c5d
Sebastian Bork <moldingbork@gmail.com>
2015-04-14 11:29:51 +0200

    indexWays & indexRelation

--- src/main/java/dk/itu/kelvin/store/ElementStore.java
+++ src/main/java/dk/itu/kelvin/store/ElementStore.java
@@ -4,26 +4,26 @@
 package dk.itu.kelvin.store;
 
 // Models
-import dk.itu.kelvin.model.Element;
-
-import dk.itu.kelvin.model.Way;
-import dk.itu.kelvin.model.Land;
-import dk.itu.kelvin.model.Relation;
-import dk.itu.kelvin.model.BoundingBox;
-
-
-// Utilities
+import dk.itu.kelvin.model.*;
+import dk.itu.kelvin.util.RectangleTree;
 import dk.itu.kelvin.util.SpatialIndex;
 
-// General utilities
-import java.util.List;
-import java.util.Collection;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+// Utilities
+// General utilities
 
 /**
  * Common store for storing all elements in the chart.
  */
 public final class ElementStore extends Store<Element, SpatialIndex.Bounds> {
+
+  /**
+   * Spatial index of ways.
+   */
+  private SpatialIndex<Way> waysTree;
 
   /**
    * A list for all land elements.
@@ -79,8 +79,41 @@ public final class ElementStore extends Store<Element, SpatialIndex.Bounds> {
    */
   @Override
   public List<Element> search(final SpatialIndex.Bounds criteria) {
+
     return null;
   }
+
+  /**
+   * Add a collection of ways to the chart.
+   *
+   * @param list The collection of ways to add to the chart.
+   */
+  public RectangleTree indexWays(final Collection<Way> list) {
+    if (ways == null || ways.isEmpty()) {
+      return null;
+    }
+
+    RectangleTree tree = new RectangleTree<Way>(list);
+    
+    return tree;
+  }
+
+  /**
+   * Add a collection of relations to the chart.
+   *
+   * @param list The relations to add to the chart.
+   */
+  public RectangleTree relations(final Collection<Relation> list) {
+    if (relations == null || relations.isEmpty()) {
+      return null;
+    }
+
+    RectangleTree tree = new RectangleTree<Relation>(list);
+
+    return tree;
+  }
+
+
 
   /**
    * Removes a specific element from the store.
