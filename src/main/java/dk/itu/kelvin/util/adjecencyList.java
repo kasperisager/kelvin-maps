525cad9281001eff12e559e1bfe03356603ce95e
djohan <hjalte55@gmail.com>
2015-04-23 11:20:37 +0200

    Beginning of adj class.

---
+++ src/main/java/dk/itu/kelvin/util/adjecencyList.java
@@ -0,0 +1,13 @@
+/**
+ * Copyright (C) 2015 The Authors.
+ */
+package dk.itu.kelvin.util;
+
+// General utilities
+import java.util.Hashtable;
+import java.util.List;
+import java.util.ArrayList;
+
+public class adjecencyList extends Hashtable {
+  
+}
