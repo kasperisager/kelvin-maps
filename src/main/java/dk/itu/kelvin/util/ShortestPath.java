526084e0aff9bc257789a99285b92232e511901b
Sebastian Bork <moldingbork@gmail.com>
2015-04-27 13:46:07 +0200

    Merge branch 'feature/adjacency-list' of /Users/sebastianbork/Desktop/ITU/2-semester/JAva/Exam/KelvinMaps with conflicts.

--- src/main/java/dk/itu/kelvin/util/ShortestPath.java
+++ src/main/java/dk/itu/kelvin/util/ShortestPath.java
@@ -5,13 +5,14 @@ package dk.itu.kelvin.util;
 
 // General utilities
 import java.util.HashMap;
-import java.util.PriorityQueue;
 
-public class ShortestPath {
+public class shortestPath {
   /**
    * Maps the distance from the source to all different vertices in the graph.
    */
   private HashMap<WeightedGraph.Node, Float> distance = new HashMap<>();
+
+  //private WeightedGraph.Edge[] edgeTo; // edgeTo[v] = last edge on shortest s->v path
 
   /**
    * Maps vertices with the last edge on the vertex's shortest path.
@@ -21,14 +22,15 @@ public class ShortestPath {
   /**
    * Priority queue holding all vertices.
    */
-  private PriorityQueue<WeightedGraph.Node> queue;
+  private Queue<Double> pq;
+
 
   /**
    * Constructor setting all distances to infinity.
    * @param graph weighted graph.
    * @param source starting point for the shortest path.
    */
-  public ShortestPath(final WeightedGraph graph, final WeightedGraph.Node source) {
+  public shortestPath(WeightedGraph graph, WeightedGraph.Node source) {
     for (WeightedGraph.Edge e : graph.edges()) {
       if (e.weight() < 0)
         throw new IllegalArgumentException("edge " + e + " has negative weight");
@@ -38,31 +40,30 @@ public class ShortestPath {
       this.distance.put(n, Float.POSITIVE_INFINITY);
       this.distance.put(source, 0.0f);
 
-      // relax vertices in order of distance from s
-      this.queue = new PriorityQueue<>(graph.V());
-      this.queue.insert(source, this.distance.get(source));
 
-      while (!queue.isEmpty()) {
-        WeightedGraph.Node v = this.queue.delMin();
+      //distTo[n] = Double.POSITIVE_INFINITY;
+      //distTo[source] = 0.0;
+
+      // relax vertices in order of distance from s
+      pq = new Queue<Double>(graph.V());
+      pq.insert(source, this.distance.get(source));
+      while (!pq.isEmpty()) {
+        WeightedGraph.No2n();
         for (WeightedGraph.Edge e : graph.neighbours(v))
           relax(e);
       }
     }
   }
 
-  // relax edge e and update queue if changed
+  // relax edge e and update pq if changed
   private void relax(WeightedGraph.Edge e) {
     WeightedGraph.Node v = e.from(), w = e.to();
     if (this.distance.get(w) > this.distance.get(v) + e.weight()) {
       this.distance.put(w, this.distance.get(v) + e.weight());
       this.edgeTo.put(w, e);
 
-      if (queue.contains(w)) {
-        this.queue.decreaseKey(w, this.distance.get(w));
-      }
-      else {
-        this.queue.insert(w, this.distance.get(w));
-      }
+      if (pq.contains(w)) pq.decreaseKey(w, this.distance.get(w));
+      else                pq.insert(w, this.distance.get(w));
     }
   }
 
@@ -72,7 +73,7 @@ public class ShortestPath {
    * @return the length of a shortest path.
    * Double.POSITIVE_INFINITY if no such path
    */
-  public double distTo(final WeightedGraph.Node v) {
+  public double distTo(WeightedGraph.Node v) {
     return this.distance.get(v);
   }
 
@@ -81,7 +82,7 @@ public class ShortestPath {
    * @param v the destination vertex.
    * @return true if there is a path, false otherwise.
    */
-  public boolean hasPathTo(final WeightedGraph.Node v) {
+  public boolean hasPathTo(WeightedGraph.Node v) {
     return this.distance.get(v) < Double.POSITIVE_INFINITY;
   }
 
@@ -91,14 +92,12 @@ public class ShortestPath {
    * @return shortest path from the source vertex to the param vertex
    * as an iterable of edges, and null if no such path.
    */
-  public Iterable<WeightedGraph.Edge> path(final WeightedGraph.Node n) {
+  public Iterable<WeightedGraph.Edge> path(WeightedGraph.Node n) {
     if (!hasPathTo(n)) return null;
-
     Stack<WeightedGraph.Edge> path = new Stack<WeightedGraph.Edge>();
-    for (WeightedGraph.Edge e = this.edgeTo.get(n); e!= null; e = this.edgeTo.get(e.from())) {
+    for (WeightedGraph.Edge e = edgeTo[n]; e != null; e = edgeTo[e.from()]) {
       path.push(e);
     }
-
     return path;
   }
 
