3f9f3421d67ef32477b1a56ff2d81a1fa323e0f2
Kasper Isager <kasperisager@gmail.com>
2015-05-04 11:35:55 +0200

    Implement stub method for object distances.

--- src/main/java/dk/itu/kelvin/util/RectangleTree.java
+++ src/main/java/dk/itu/kelvin/util/RectangleTree.java
@@ -397,6 +397,24 @@ public class RectangleTree<E extends RectangleTree.Index>
   }
 
   /**
+   * Find the actual distance from an element to a point.
+   *
+   * @param point   The point to find the distance to.
+   * @param element The element to find the distance from.
+   * @return        The actual distance from the element to the point.
+   */
+  private static <E extends Index> double actualDistance(
+    final Point point,
+    final E element
+  ) {
+    if (point == null || element == null) {
+      return Double.POSITIVE_INFINITY;
+    }
+
+    return RectangleTree.minimumDistance(point, element.bounds());
+  }
+
+  /**
    * The {@link Index} interface describes an object that is indexable by the
    * rectangle tree.
    */
@@ -740,7 +758,7 @@ public class RectangleTree<E extends RectangleTree.Index>
       // contain the NN (theorems 1 and 2). We use this in downward pruning."
       for (int i = 0; i < abl.size(); i++) {
         double minimumDistance = RectangleTree.minimumDistance(
-          point, abl.get(i)
+          point, abl.get(i).bounds()
         );
 
         if (minimumDistance > minimumMinimaxDistance) {
@@ -767,12 +785,8 @@ public class RectangleTree<E extends RectangleTree.Index>
           nearest = estimate;
         }
         else {
-          double distNearest = RectangleTree.minimumDistance(
-            point, nearest.bounds()
-          );
-          double distEstimate = RectangleTree.minimumDistance(
-            point, estimate.bounds()
-          );
+          double distNearest = RectangleTree.actualDistance(point, nearest);
+          double distEstimate = RectangleTree.actualDistance(point, estimate);
 
           if (distNearest > distEstimate) {
             nearest = estimate;
