fb15451fed96afafddf0d24211fb20efb9f0a82e
djohan <hjalte55@gmail.com>
2015-05-11 17:41:33 +0200

    Small changes.

--- src/test/java/dk/itu/kelvin/math/HaversineTest.java
+++ src/test/java/dk/itu/kelvin/math/HaversineTest.java
@@ -6,9 +6,8 @@ package dk.itu.kelvin.math;
 // JUnit annotations
 import org.junit.Test;
 
-import static org.junit.Assert.assertTrue;
-
 // JUnit assertions
+import static org.junit.Assert.assertTrue;
 
 /**
  * {@link Haversine} test suite.
@@ -17,7 +16,7 @@ import static org.junit.Assert.assertTrue;
  * Test the distance between two specified coordinates. The results are calculated
  * on http://andrew.hedges.name/experiments/haversine/.
  *
- * We set a a deviation on 2 %.
+ * We accept a deviation of 2 %.
  * <p>
  *
  */
@@ -34,8 +33,7 @@ public final class HaversineTest {
     final double lat2 = 55.675637;
     final double lon2 = 12.569544;
 
-
-    // we set a deviation on 2 %.
+    // we accept a deviation of 2 %.
     double realDist1 = 2.216 * 1.01;
     double realDist2 = 2.216 * 0.99;
     double ourDist = Haversine.distance(lat1, lon1, lat2, lon2) * 0.99;
@@ -49,7 +47,7 @@ public final class HaversineTest {
     final double lat4 = 24.15;
     final double lon4 = 76.0;
 
-    // we set a deviation on 2 %.
+    // we accept a deviation of 2 %.
     double realDist3 = 6159.197 * 1.02;
     double realDist4 = 6159.197 * 0.98;
     double ourDist1 = Haversine.distance(lat3, lon3, lat4, lon4) * 0.99;
@@ -57,8 +55,5 @@ public final class HaversineTest {
     assertTrue( ourDist1 < realDist3);
     assertTrue( ourDist1 > realDist4);
   }
-
-
-
 
 }
