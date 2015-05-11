67afaaf2db640ff55b5d906028e483fffd099871
Sebastian Bork <moldingbork@gmail.com>
2015-05-11 15:14:54 +0200

    RelationTest up to date

--- src/test/java/dk/itu/kelvin/model/RelationTest.java
+++ src/test/java/dk/itu/kelvin/model/RelationTest.java
@@ -62,29 +62,4 @@ public final class RelationTest {
     Way w2 = new Way();
     r1.add(w2);
   }
-
-  /**
-   * A relation should be able to consist of one or more member elements.
-   */
- /* @Test
-  public void testRelationListOfMembers() {
-    // Test creation of relation with a number of members.
-    Relation r1 = new Relation();
-    Node n1 = new Node(3, 3);
-    Node n2 = new Node(4, 4);
-    Node n3 = new Node(5, 5);
-
-    r1.add(n1);
-    r1.add(n2);
-    r1.add(n3);
-
-    assertEquals(3, r1.members().size());
-    assertTrue(r1.members().contains(n1));
-    assertTrue(r1.members().contains(n2));
-    assertTrue(r1.members().contains(n3));
-
-    // Test creation of relation without members.
-    Relation r2 = new Relation();
-    assertEquals(0, r2.members().size());
-  }*/
 }
